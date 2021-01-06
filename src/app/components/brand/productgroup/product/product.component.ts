754ee16147ebde5980d643fa892460af07954b8f
AdharshSabu <adharshsabu7@gmail.com>
2021-01-07 00:26:58 +0530

    revised UI

--- src/app/components/brand/productgroup/product/product.component.ts
+++ src/app/components/brand/productgroup/product/product.component.ts
@@ -14,14 +14,71 @@ export class ProductComponent implements OnInit {
   url ='./assets/L.json';
   items=[];
 
+  subcategories=[];
+  s1=[];
+  mime: string;
+  renditions=[];
+  format=[];
+  ur=[];
+
   constructor(private data: MessageService,
               private http: HttpClient) { 
                 this.http.get(this.url).toPromise().then(data => {console.log(data);
                 
                   for (let key in data){
                     if (data.hasOwnProperty(key)){
-                      this.items.push(data[key]);
+                      if (key=="startingPrice"){}
+                      else if(key=="media"){
+                        //console.log(data[key]["icon"]["renditions"]);
+                        // for (let i of data[key]){
+                        //   console.log(i);
+                        // }
+                        this.mime=data[key]["icon"]["mime"];
+                        this.renditions.push(data[key]["icon"]["renditions"]);
+                      }
+                      else if ( key=="subcategories"){
+                        this.subcategories.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                      else {
+                        this.items.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
                     }
+                  }
+                  for (let i of this.renditions){
+                    for (let j of i){
+                      
+                      for (let key in j){
+                        if (key=="format"){
+                          this.format.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                        else if (key=="url"){
+                          this.ur.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                      }
+                    }
+                  }
+            
+                  for (let i of this.subcategories){
+                    for (let j of i){
+                      console.log(j);
+                      for (let key in j){
+                    
+                        //if (j.hasOwnProperty(key)){
+                          this.s1.push(j[key]);
+                          console.log(key);
+                          console.log(j[key]);
+                        //}
+                      }
+                    }
+                    //this.s1.push(data[i]);
+                    //console.log(i);
+                    //console.log(data[key]);
                   }
                 
                 })
--- src/app/components/brand/productgroup/summary/summary.component.ts
+++ src/app/components/brand/productgroup/summary/summary.component.ts
@@ -13,6 +13,13 @@ export class SummaryComponent implements OnInit {
   fname :string;
   url : string;
   items=[];
+
+  subcategories=[];
+  s1=[];
+  mime: string;
+  renditions=[];
+  format=[];
+  ur=[];
   constructor(private data: MessageService,
               private http: HttpClient) { 
                 
@@ -28,9 +35,59 @@ export class SummaryComponent implements OnInit {
                 
         for (let key in data){
           if (data.hasOwnProperty(key)){
-            this.items.push(data[key]);
-          }
-        }
+            if (key=="startingPrice"){}
+                      else if(key=="media"){
+                        //console.log(data[key]["icon"]["renditions"]);
+                        // for (let i of data[key]){
+                        //   console.log(i);
+                        // }
+                        this.mime=data[key]["icon"]["mime"];
+                        this.renditions.push(data[key]["icon"]["renditions"]);
+                      }
+                      else if ( key=="subcategories"){
+                        this.subcategories.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                      else {
+                        this.items.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                    }
+                  }
+                  for (let i of this.renditions){
+                    for (let j of i){
+                      
+                      for (let key in j){
+                        if (key=="format"){
+                          this.format.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                        else if (key=="url"){
+                          this.ur.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                      }
+                    }
+                  }
+            
+                  for (let i of this.subcategories){
+                    for (let j of i){
+                      console.log(j);
+                      for (let key in j){
+                    
+                        //if (j.hasOwnProperty(key)){
+                          this.s1.push(j[key]);
+                          console.log(key);
+                          console.log(j[key]);
+                        //}
+                      }
+                    }
+                    //this.s1.push(data[i]);
+                    //console.log(i);
+                    //console.log(data[key]);
+                  }
       
       })
     }
@@ -41,9 +98,59 @@ export class SummaryComponent implements OnInit {
                 
         for (let key in data){
           if (data.hasOwnProperty(key)){
-            this.items.push(data[key]);
-          }
-        }
+            if (key=="startingPrice"){}
+                      else if(key=="media"){
+                        //console.log(data[key]["icon"]["renditions"]);
+                        // for (let i of data[key]){
+                        //   console.log(i);
+                        // }
+                        this.mime=data[key]["icon"]["mime"];
+                        this.renditions.push(data[key]["icon"]["renditions"]);
+                      }
+                      else if ( key=="subcategories"){
+                        this.subcategories.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                      else {
+                        this.items.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                    }
+                  }
+                  for (let i of this.renditions){
+                    for (let j of i){
+                      
+                      for (let key in j){
+                        if (key=="format"){
+                          this.format.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                        else if (key=="url"){
+                          this.ur.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                      }
+                    }
+                  }
+            
+                  for (let i of this.subcategories){
+                    for (let j of i){
+                      console.log(j);
+                      for (let key in j){
+                    
+                        //if (j.hasOwnProperty(key)){
+                          this.s1.push(j[key]);
+                          console.log(key);
+                          console.log(j[key]);
+                        //}
+                      }
+                    }
+                    //this.s1.push(data[i]);
+                    //console.log(i);
+                    //console.log(data[key]);
+                  }
       
       })
     }
@@ -54,9 +161,59 @@ export class SummaryComponent implements OnInit {
                 
         for (let key in data){
           if (data.hasOwnProperty(key)){
-            this.items.push(data[key]);
-          }
-        }
+            if (key=="startingPrice"){}
+                      else if(key=="media"){
+                        //console.log(data[key]["icon"]["renditions"]);
+                        // for (let i of data[key]){
+                        //   console.log(i);
+                        // }
+                        this.mime=data[key]["icon"]["mime"];
+                        this.renditions.push(data[key]["icon"]["renditions"]);
+                      }
+                      else if ( key=="subcategories"){
+                        this.subcategories.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                      else {
+                        this.items.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                    }
+                  }
+                  for (let i of this.renditions){
+                    for (let j of i){
+                      
+                      for (let key in j){
+                        if (key=="format"){
+                          this.format.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                        else if (key=="url"){
+                          this.ur.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                      }
+                    }
+                  }
+            
+                  for (let i of this.subcategories){
+                    for (let j of i){
+                      console.log(j);
+                      for (let key in j){
+                    
+                        //if (j.hasOwnProperty(key)){
+                          this.s1.push(j[key]);
+                          console.log(key);
+                          console.log(j[key]);
+                        //}
+                      }
+                    }
+                    //this.s1.push(data[i]);
+                    //console.log(i);
+                    //console.log(data[key]);
+                  }
       
       })
     }
@@ -67,9 +224,59 @@ export class SummaryComponent implements OnInit {
                 
         for (let key in data){
           if (data.hasOwnProperty(key)){
-            this.items.push(data[key]);
-          }
-        }
+            if (key=="startingPrice"){}
+                      else if(key=="media"){
+                        //console.log(data[key]["icon"]["renditions"]);
+                        // for (let i of data[key]){
+                        //   console.log(i);
+                        // }
+                        this.mime=data[key]["icon"]["mime"];
+                        this.renditions.push(data[key]["icon"]["renditions"]);
+                      }
+                      else if ( key=="subcategories"){
+                        this.subcategories.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                      else {
+                        this.items.push(data[key]);
+                        console.log(key);
+                        console.log(data[key]);
+                      }
+                    }
+                  }
+                  for (let i of this.renditions){
+                    for (let j of i){
+                      
+                      for (let key in j){
+                        if (key=="format"){
+                          this.format.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                        else if (key=="url"){
+                          this.ur.push(j[key]);
+                          //console.log(j[key]);
+                        }
+                      }
+                    }
+                  }
+            
+                  for (let i of this.subcategories){
+                    for (let j of i){
+                      console.log(j);
+                      for (let key in j){
+                    
+                        //if (j.hasOwnProperty(key)){
+                          this.s1.push(j[key]);
+                          console.log(key);
+                          console.log(j[key]);
+                        //}
+                      }
+                    }
+                    //this.s1.push(data[i]);
+                    //console.log(i);
+                    //console.log(data[key]);
+                  }
       
       })
     }
